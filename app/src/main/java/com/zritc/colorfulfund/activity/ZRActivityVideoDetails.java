ac9c37155504785981c9de2cc32ccc1039ece19d
xiaochang <qianchang.xiao@gmail.com>
2016-08-25 00:51:48 +0800

    视频接入videojj

--- app/src/main/java/com/zritc/colorfulfund/activity/ZRActivityVideoDetails.java
+++ app/src/main/java/com/zritc/colorfulfund/activity/ZRActivityVideoDetails.java
@@ -1,12 +1,16 @@
 package com.zritc.colorfulfund.activity;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.MediaController;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.VideoView;
 
@@ -21,6 +25,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.Bind;
+import cn.com.video.venvy.param.JjVideoRelativeLayout;
+import cn.com.video.venvy.param.JjVideoView;
+import cn.com.video.venvy.param.OnJjBufferCompleteListener;
+import cn.com.video.venvy.param.OnJjBufferStartListener;
+import cn.com.video.venvy.param.OnJjBufferingUpdateListener;
+import cn.com.video.venvy.param.OnJjOnOpenFailedListener;
+import cn.com.video.venvy.param.OnJjOpenStartListener;
+import cn.com.video.venvy.param.OnJjOpenSuccessListener;
+import cn.com.video.venvy.param.OnJjOutsideLinkClickListener;
+import cn.com.video.venvy.param.VideoJjMediaContoller;
 
 /**
  * 视频详情界面
@@ -31,8 +45,20 @@ import butterknife.Bind;
  */
 public class ZRActivityVideoDetails extends ZRActivityToolBar<VideoDetailsPresenter> implements IVideoDetailsView {
 
-    @Bind(R.id.vv_video_view)
-    VideoView mVideoView;
+    @Bind(R.id.jj_video_view)
+    JjVideoView mVideoView;
+
+    @Bind(R.id.sdk_ijk_progress_bar_layout)
+    View mLoadView;
+
+    @Bind(R.id.sdk_load_layout)
+    View mLoadBufferView;
+
+    @Bind(R.id.sdk_ijk_progress_bar_text)
+    TextView mLoadText;
+
+    @Bind(R.id.sdk_sdk_ijk_load_buffer_text)
+    TextView mLoadBufferTextView;
 
     @Bind(R.id.sc_scrollView)
     PullToZoomScrollViewEx mScrollView;
@@ -71,6 +97,7 @@ public class ZRActivityVideoDetails extends ZRActivityToolBar<VideoDetailsPresen
     private HotVideoAdapter adapter;
     private List<HotVideo> datas;
     private String uriString = "https://test-media-cmcaifu-com.oss-cn-hangzhou.aliyuncs.com/upload/1781fd560180400b971bd9c08afa0e0f.mp4";
+    String mVideoUrl = "http://v.youku.com/v_show/id_XMTUzNzM1MjUwMA==_ev_5.html?from=y1.3-idx-uhome-1519-20887.205805-205902.8-1";
 
     @Override
     protected int getContentViewId() {
@@ -90,11 +117,119 @@ public class ZRActivityVideoDetails extends ZRActivityToolBar<VideoDetailsPresen
         adapter = new HotVideoAdapter(this, datas, R.layout.lv_video_detail_item);
         mHotVideo.setAdapter(adapter);
 
-        Uri uri = Uri.parse(uriString);
-        mVideoView.setMediaController(new MediaController(this));
-        mVideoView.setVideoURI(uri);
-        mVideoView.requestFocus();
-        mVideoView.start();
+        mVideoView.setMediaController(new VideoJjMediaContoller(this, true));
+        mLoadBufferTextView.setTextColor(Color.RED);
+        /***
+         * 用户自定义的外链 可 获取外链点击时间
+         */
+        mVideoView
+                .setOnJjOutsideLinkClickListener(new OnJjOutsideLinkClickListener() {
+
+                    @Override
+                    public void onJjOutsideLinkClick(String arg0) {
+                        // TODO Auto-generated method stub
+                    }
+
+                    @Override
+                    public void onJjOutsideLinkClose() {
+                        // TODO Auto-generated method stub
+
+                    }
+                });
+        /***
+         * 设置缓冲
+         */
+        mVideoView.setMediaBufferingView(mLoadBufferView);
+        /***
+         * 视频开始加载数据
+         */
+        mVideoView.setOnJjOpenStart(new OnJjOpenStartListener() {
+
+            @Override
+            public void onJjOpenStart(String arg0) {
+                mLoadText.setText(arg0);
+            }
+        });
+        /***
+         * 视频开始播放
+         */
+        mVideoView.setOnJjOpenSuccess(new OnJjOpenSuccessListener() {
+
+            @Override
+            public void onJjOpenSuccess() {
+                mLoadView.setVisibility(View.GONE);
+            }
+        });
+        // 缓冲开始
+        mVideoView.setOnJjBufferStart(new OnJjBufferStartListener() {
+
+            @Override
+            public void onJjBufferStartListener(int arg0) {
+                Log.e("Video++", "====================缓冲值=====" + arg0);
+            }
+        });
+        mVideoView
+                .setOnJjBufferingUpdateListener(new OnJjBufferingUpdateListener() {
+
+                    @Override
+                    public void onJjBufferingUpdate(int arg1) {
+                        //						mLoadView.setVisibility(mVideoView
+                        //								.getBufferPercentage() > 5 ? View.GONE
+                        //								: View.VISIBLE);
+                        if (mLoadBufferView.getVisibility() == View.VISIBLE) {
+                            mLoadBufferTextView.setText(String
+                                    .valueOf(mVideoView.getBufferPercentage())
+                                    + "%");
+                            Log.e("Video++", "====================缓冲值2====="
+                                    + arg1);
+                        }
+                    }
+                });
+        // 缓冲完成
+        mVideoView.setOnJjBufferComplete(new OnJjBufferCompleteListener() {
+
+            @Override
+            public void onJjBufferCompleteListener(int arg0) {
+                // TODO Auto-generated method stub
+                mLoadView.setVisibility(View.GONE);
+            }
+        });
+        mVideoView.setOnJjOpenFailedListener(new OnJjOnOpenFailedListener(){
+
+            @Override
+            public boolean onJjOpenFailed(int arg0, int arg1) {
+                return false;
+            }
+
+        });
+        /***
+         * 注意VideoView 要调用下面方法 配置你用户信息
+         */
+        mVideoView.setVideoJjAppKey("N1WlKQabW");
+        mVideoView.setVideoJjPageName("com.example.videojjsdkdemo");
+        // mVideoView.setMediaCodecEnabled(true);// 是否开启 硬解 硬解对一些手机有限制
+        /***
+         * 视频标签显示的时间 默认显示5000毫秒 可设置 传入值 long类型 毫秒
+         */
+        // 参数代表是否记录视频播放位置 默认false不记录 true代表第二次或多次进入，直接跳转到上次退出的时间点开始播放
+        // mVideoView.setVideoJjSaveExitTime(false);
+        /***
+         * 指定时间开始播放 毫秒
+         */
+        // mVideoView.setVideoJjSeekToTime(Long.valueOf(20000));
+        mVideoView.setVideoJjResetState();
+        // 判断是否源
+        // 0：代表 8大视频网站url
+        // 3：代表自己服务器的视频源
+        // 2：代表直播地址
+        // 1：代表本地视频(手机上的视频源)
+        // 4：特殊需求
+        mVideoView.setVideoJjType(0);
+        mVideoView.setResourceVideo(mVideoUrl);
+        RelativeLayout mLayout = (RelativeLayout) findViewById(R.id.root);
+        JjVideoRelativeLayout mJjVideoRelativeLayout = (JjVideoRelativeLayout) findViewById(R.id.jjlayout);
+        mJjVideoRelativeLayout.setJjToFront(mLayout);// 设置此方法
+        // 重新排序视图层级JjVideoRelativeLayout，避免横屏其它遮挡
     }
 
     private void initData() {
@@ -121,6 +256,14 @@ public class ZRActivityVideoDetails extends ZRActivityToolBar<VideoDetailsPresen
     @Override
     public void onError(String msg) {
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 必须调用 要不直播有问题
+        if (mVideoView != null)
+            mVideoView.onDestroy();
     }
 
     static class HotVideoAdapter extends ZRCommonAdapter<HotVideo> {
